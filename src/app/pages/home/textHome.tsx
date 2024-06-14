080fd027d9d11926abecae42c61069b4bfae9c13
citakurnia <citakurniah@gmail.com>
2024-06-15 03:13:49 +0700

    Change text to fic react/no-unescaped-entities error

--- src/app/pages/home/textHome.tsx
+++ src/app/pages/home/textHome.tsx
@@ -6,9 +6,9 @@ export default function TextHome() {
         Unveil the beginnings of my innovative developer journey!
       </p>
       <p className="mt-4 text-gray-700">
-        I'm a budding Full-Stack Developer who started this year. Though new to
-        the field, my passion for learning and dedication has propelled me to
-        quickly grasp essential skills. I am dedicated to continuous
+        I&apos;m a budding Full-Stack Developer who started this year. Though
+        new to the field, my passion for learning and dedication has propelled
+        me to quickly grasp essential skills. I am dedicated to continuous
         improvement, striving to create effective and efficient solutions on any
         project.
       </p>
