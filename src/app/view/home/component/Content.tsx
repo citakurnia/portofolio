faece8383215347d0441dd197f0938b534712217
citakurnia <citakurniah@gmail.com>
2024-10-25 18:54:04 +0700

    fix: change content

--- src/app/view/home/component/Content.tsx
+++ src/app/view/home/component/Content.tsx
@@ -5,16 +5,17 @@ export default function Content() {
     <div>
       <h1 className="text-4xl font-bold">Hello</h1>
       <p className="mt-4 text-gray-900">
-        I&apos;m Cita, a software engineer with a strong aptitude for logical
-        thinking, problem-solving, and adapting to new technologies. While
-        I&apos;m still early in my career, I&apos;ve quickly gained the ability
-        to work across various areas of software engineering, including backend,
-        frontend, and full stack development.
+        I&apos;m Cita, a software engineer with a passion for logical thinking,
+        problem-solving, and learning new technologies. Although I&apos;m still
+        in the early stages of my career, I&apos;ve quickly developed skills
+        across various areas of software development, including backend,
+        frontend, and full stack work.
       </p>
       <p className="mt-4 mb-2 text-gray-700">
-        My extroverted and respectful nature fosters positive collaboration in
-        team environments, and I&apos;m always eager to take on new challenges,
-        grow my skills, and contribute to innovative solutions.
+        It wasn&apos;t until recently that I discovered a passion for software
+        engineering, with a particular interest in backend development. I&apos;m
+        excited to continue expanding my skills, take on new challenges, and
+        contribute to impactful projects through collaboration and innovation.
       </p>
       <Link
         href="/project"
