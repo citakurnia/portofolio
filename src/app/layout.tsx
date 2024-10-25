b4d729a817a756cd86d2a84069cb76db65a69f0c
citakurnia <citakurniah@gmail.com>
2024-10-25 18:52:11 +0700

    fix: change font and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
+const defaultFont = Lato({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CV Portofolio Cita Kurnia",
-  description: "Resume of Cita Kurnia Fullstack Web Developer Position",
+  title: "Cita Kurnia | Portfolio Website",
+  description:
+    "Welcome to the personal portfolio of Cita Kurnia, a dedicated Software Engineer - Full Stack Developer with a passion for building innovative solutions.",
 };
 
 export default function RootLayout({
@@ -18,13 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={defaultFont.className}>
         <div className="relative flex flex-col min-h-screen bg-primary-soft-pink z-20">
-          <div className="absolute inset-0 bg-gradient-to-r from-primary-med-blue to-primary-pink w-full h-full md:w-2/5"></div>
+          {/* <div className="absolute inset-0 bg-gradient-to-r from-primary-med-blue to-primary-pink w-full h-full md:w-2/5" /> */}
           <div className="relative bg-primary-soft-pink w-full z-50 mx-auto px-4 py-3 md:py-5 lg:px-10">
             <Header />
           </div>
-          <main className="relative flex items-center justify-center flex-grow mx-auto px-4 py-8 z-10">
+          <main className="relative flex flex-grow mx-auto px-4 py-8 z-10">
             {children}
           </main>
           <div className="relative bg-primary-soft-pink w-full mx-auto px-4 py-4 md:py-5 z-10 lg:px-10">
