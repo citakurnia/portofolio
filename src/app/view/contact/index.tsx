2aeeeadf0caf80a63379e845be28f19ed2670bac
citakurnia <citakurniah@gmail.com>
2024-10-30 01:52:03 +0700

    fix: change overall contact view

--- src/app/view/contact/index.tsx
+++ src/app/view/contact/index.tsx
@@ -1,54 +1,69 @@
 import { SvgEmail, SvgLinkedIn } from "@/assets/svg-collection";
+import { Button } from "@/components/ui/button";
 
 export default function ContactView() {
   const svgClassName =
-    "text-primary-deep-blue/50 hover:text-primary-deep-blue/80";
+    "text-primary-deep-blue/70 hover:text-primary-deep-blue/80 flex flex-row gap-4";
   return (
     <>
       <div className="flex flex-col md:flex-row justify-center items-center container mx-auto">
-        <div className="relative z-10 w-[80vw]">
-          <div className="relative w-full p-4 py-8 md:py-12 rounded-lg sm:rounded-full shadow-lg bg-primary-med-blue/20">
+        <div className="relative z-10 w-[70vw] md:w-[60vw] lg:w-[50vw] xl:w-[40vw]">
+          <div className="relative w-full p-4 py-8 rounded-lg shadow-lg bg-primary-med-blue/20">
             <div className="font-semibold text-xl sm:text-2xl text-center">
-              Contact Here
+              My Contact
             </div>
             <p className="text-sm mt-2 text-black/80 text-center">
               Feel free to reach out for collaboration or inquiries!
             </p>
             <div>
-              <div className="flex flex-col space-y-6 sm:space-y-0 sm:flex-row justify-between px-4 sm:px-12 md:px-28 xl:px-72 pt-4">
-                <div className="flex flex-col items-center">
-                  <a
-                    aria-label="Send email to Cita Kurnia with gmail"
-                    href="https://mail.google.com/mail/?view=cm&to=citakurniah@gmail.com"
-                    target="_blank"
-                    className={svgClassName}
+              <div className="flex flex-col space-y-6 sm:space-y-0 sm:flex-row justify-between px-4 sm:px-12 pt-4">
+                <div className="flex flex-col items-center gap-2">
+                  <Button
+                    variant="outline"
+                    className="w-full flex items-center justify-center space-x-2 bg-white/60"
+                    asChild
                   >
-                    <SvgEmail
-                      width={100}
-                      height={100}
-                      fill="none"
-                      className="hover: fill-current"
-                    />
-                  </a>
-                  <div className="text-black/60 text-sm">
+                    <a
+                      aria-label="Send email to Cita Kurnia with gmail"
+                      href="https://mail.google.com/mail/?view=cm&to=citakurniah@gmail.com"
+                      target="_blank"
+                      className={svgClassName}
+                    >
+                      <div className="font-medium">Email</div>
+                      <SvgEmail
+                        width={25}
+                        height={25}
+                        fill="none"
+                        className="hover: fill-current"
+                      />
+                    </a>
+                  </Button>
+                  <div className="text-black/70 text-sm">
                     citakurniah@gmail.com
                   </div>
                 </div>
-                <div className="flex flex-col items-center">
-                  <a
-                    aria-label="Direct to Cita Kurnia's LinkedIn"
-                    href="https://www.linkedin.com/in/cita-kurnia-hermaningsih-004675164/"
-                    target="_blank"
-                    className={svgClassName}
+                <div className="flex flex-col items-center gap-2">
+                  <Button
+                    variant="outline"
+                    className="w-full flex items-center justify-center space-x-2 bg-white/60"
+                    asChild
                   >
-                    <SvgLinkedIn
-                      width={100}
-                      height={100}
-                      fill="none"
-                      className="hover: fill-current"
-                    />
-                  </a>
-                  <div className="text-black/60 text-sm">
+                    <a
+                      aria-label="Direct to Cita Kurnia's LinkedIn"
+                      href="https://www.linkedin.com/in/citakurnia/"
+                      target="_blank"
+                      className={svgClassName}
+                    >
+                      <div className="font-medium">LinkedIn</div>
+                      <SvgLinkedIn
+                        width={25}
+                        height={25}
+                        fill="none"
+                        className="hover: fill-current"
+                      />
+                    </a>
+                  </Button>
+                  <div className="text-black/70 text-sm">
                     Cita Kurnia Hermaningsih
                   </div>
                 </div>
