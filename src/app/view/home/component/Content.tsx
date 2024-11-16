a46a32ff73408defe1e0f80448fea37e5c73dfdd
citakurnia <citakurniah@gmail.com>
2024-11-16 19:17:36 +0700

    fix: change home content

--- src/app/view/home/component/Content.tsx
+++ src/app/view/home/component/Content.tsx
@@ -6,20 +6,19 @@ export default function Content() {
       <h1 className="text-4xl font-bold">Hello</h1>
       <p className="mt-4 text-gray-900">
         I&apos;m Cita, a software engineer with a passion for logical thinking,
-        problem-solving, and learning new technologies. Although I&apos;m still
-        in the early stages of my career, I&apos;ve quickly developed skills
-        across various areas of software development, including backend,
-        frontend, and full stack work.
+        problem-solving, and learning new technologies. With almost a year of
+        experience in full-stack development, I focus primarily on backend
+        development.
       </p>
       <p className="mt-4 mb-2 text-gray-700">
-        It wasn&apos;t until recently that I discovered a passion for software
-        engineering, with a particular interest in backend development. I&apos;m
-        excited to continue expanding my skills, take on new challenges, and
-        contribute to impactful projects through collaboration and innovation.
+        Driven by curiosity and a dedication to continuous learning, I work on
+        upgrading my skills regularly. I&apos;m excited to continue expanding my
+        skills, take on new challenges, and contribute to impactful projects
+        through collaboration and innovation.
       </p>
       <Link
         href="/project"
-        className="text-md text-primary-deep-blue hover:text-primary-med-blue"
+        className="text-md text-primary-deep-blue hover:text-primary-med-blue mt-2"
       >
         Discover my projects here.
       </Link>
